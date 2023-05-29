5f5050f180de56c50a68d8b0a16d7ad7eef673fe
tobeh <tobiasch@gmx.at>
2023-05-29 23:58:30 +0200

    add scenes and rainbow mod

---
+++ sprites/rainbow/index.php
@@ -0,0 +1,23 @@
+<?php
+
+$sprite = substr(parse_url($_GET["url"], PHP_URL_PATH),1);
+//echo $sprite;
+$hue = $_GET["hue"];
+
+
+function modulateImage($imagePath, $hue, $brightness, $saturation) {
+    $imagick = new \Imagick(realpath($imagePath));
+    $imagick = $imagick->coalesceImages();
+
+    do {
+      $imagick->modulateImage($brightness, $saturation, $hue);
+    } while( $imagick->nextImage() );
+
+    //$imagick = $imagick->deconstructImages();
+    header("Content-Type: image/gif");
+    echo $imagick->getImagesBlob();
+}
+
+modulateImage($sprite, $hue, 100, 100);
+
+?>
