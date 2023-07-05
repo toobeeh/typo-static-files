32cfeef18d6a20133654fb5507199b863e86f3b2
tobeh <tobiasch@gmx.at>
2023-07-05 16:47:58 +0200

    fix rainbow sprite script

--- sprites/rainbow/index.php
+++ sprites/rainbow/index.php
@@ -1,22 +1,25 @@
 <?php
 
-$sprite = substr(parse_url($_GET["url"], PHP_URL_PATH),1);
-//echo $sprite;
-$hue = $_GET["hue"];
+  /* get path without URL and hue param */
+  $sprite = substr(parse_url($_GET["url"], PHP_URL_PATH),1);
+  $hue = $_GET["hue"];
 
-function modulateImage($imagePath, $hue, $brightness, $saturation) {
-    $imagick = new \Imagick(realpath($imagePath));
-    $imagick = $imagick->coalesceImages();
+  function modulateImage($imagePath, $hue, $brightness, $saturation) {
 
-    do {
-      $imagick->modulateImage($brightness, $saturation, $hue);
-    } while( $imagick->nextImage() );
+      $imagick = new Imagick(realpath($imagePath));
+      $imagick = $imagick->coalesceImages();
 
-    //$imagick = $imagick->deconstructImages();
-    header("Content-Type: image/gif");
-    echo $imagick->getImagesBlob();
-}
+      do {
+        $imagick->modulateImage($brightness, $saturation, $hue);
+      } while( $imagick->nextImage() );
 
-modulateImage($sprite, $hue, 100, 100);
+      //$imagick = $imagick->deconstructImages();
+      header("Content-Type: image/gif");
+      echo $imagick->getImagesBlob();
+  }
+
+  /* make path relative to webroot and process image*/
+  $sprite = "../../" .  $sprite;
+  modulateImage($sprite, $hue, 100, 100);
 
 ?>
